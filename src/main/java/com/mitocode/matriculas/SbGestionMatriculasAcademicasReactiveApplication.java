7daaec2f08038e0c662c79c6c7c8ce9cd6d47fe9
RodrigoR <r4mirez.r@gmail.com>
2024-09-08 17:32:59 -0500

    feat(Create and Conf. webflux): Creación y conf para trabajar con webflux

--- src/main/java/com/mitocode/matriculas/SbGestionMatriculasAcademicasReactiveApplication.java
+++ src/main/java/com/mitocode/matriculas/SbGestionMatriculasAcademicasReactiveApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbGestionMatriculasAcademicasReactiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbGestionMatriculasAcademicasReactiveApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SbGestionMatriculasAcademicasReactiveApplication.class, args);
+    }
 
 }
