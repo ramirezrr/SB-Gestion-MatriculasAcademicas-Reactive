6b6c7971e72b2f43ac989027795ad11b072809cf
RodrigoR <r4mirez.r@gmail.com>
2024-09-08 17:27:49 -0500

    Initial commit”

---
+++ src/main/java/com/mitocode/matriculas/SbGestionMatriculasAcademicasReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode.matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGestionMatriculasAcademicasReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGestionMatriculasAcademicasReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitocode/matriculas/SbGestionMatriculasAcademicasReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGestionMatriculasAcademicasReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
